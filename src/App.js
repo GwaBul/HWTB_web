4bfdab9a3049f06d005de848160e17e6f659c976
JS-Yang <117419920+YJunSuk@users.noreply.github.com>
2024-05-13 00:21:20 +0900

    헤더, GPS 버튼 완성

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import { initializeApp } from "firebase/app";
 import { firebaseConfig, vapidKey, apikey } from './config';
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 import { useGeoLocation } from './hooks/useGeoLocation';
-
 import axios from "axios"
 import './App.css';
-import { Button } from '@mui/material';
-import Header from './container/Header';
-import LocationButton from './container/LocationButton';
+import Header from './components/HeaderComponent';
+import LocationButton from './components/LocationButtonComponent';
+
 const { Tmapv3 } = window;
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
@@ -151,46 +150,6 @@ const App = () => {
     if (map && location) {
       const center = new Tmapv3.LatLng(parseFloat(latitude), parseFloat(longitude));
       map.setCenter(center);
-    }
-  };
-
-  const navigateToUserLocation = async (e) => {
-    try {
-      const response = await axios.post(
-        'https://apis.openapi.sk.com/tmap/routes/pedestrian?version=1&callback=function',
-        requestData,
-        { apikey }
-      );
-
-      const resultData = response.data.features;
-      const drawInfoArr = [];
-
-      for (let i in resultData) {
-        const geometry = resultData[i].geometry;
-        if (geometry.type === 'LineString') {
-          for (let j in geometry.coordinates) {
-            const latlng = new Tmapv3.Point(
-              geometry.coordinates[j][0],
-              geometry.coordinates[j][1]
-            );
-            const convertPoint = new Tmapv3.Projection.convertEPSG3857ToWGS84GEO(latlng);
-            const convertChange = new Tmapv3.LatLng(convertPoint._lat, convertPoint._lng);
-            drawInfoArr.push(convertChange);
-          }
-        }
-      }
-      drawLine(drawInfoArr);
-
-      function drawLine(arrPoint) {
-        new Tmapv3.Polyline({
-          path: arrPoint,
-          strokeColor: "#34ff8b",
-          strokeWeight: 6,
-          map: map
-        });
-      }
-    } catch (error) {
-      console.error('Error navigating to user location:', error);
     }
   };
 
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Button, Card, CardContent, Collapse, Typography } from '@mui/material';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
-import './css/Header.css'
+import '../components/css/header.css'
 
 const Header = () => {
     const [expanded, setExpanded] = useState(false);
@@ -13,9 +13,9 @@ const Header = () => {
 
     return (
         <div className="header">
-            <Card className="cardStyle" sx={{ position: 'relative', justifyContent: 'center', zIndex: '999', maxWidth: 600, marginLeft: 7 }}>
+            <Card className="cardStyle" sx={{ position: 'relative', justifyContent: 'center', zIndex: '999', maxWidth: 600, marginLeft: 7,  borderRadius:5 }}>
                 <div style={{display:'flex'}}>
-                    <Typography variant='h5' sx={{ fontFamily: "NanumBarunGothicBold", display: 'flex', alignItems: 'center', justifyContent: 'space-between', px: 2, py: 1, pr: 0 }}>
+                    <Typography variant='h5' sx={{ fontFamily: "NanumBarunGothicBold", display: 'flex', alignItems: 'center', justifyContent: 'space-between', px: 4, py: 1.5, pr: 0}}>
                         실시간 하천 수위 확인
                     </Typography>
                     <Button onClick={handleExpandClick} sx={{ width: 15, px: 0 }}>{expanded ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}</Button>
@@ -23,7 +23,7 @@ const Header = () => {
                 <Collapse in={expanded} timeout="auto" unmountOnExit>
                     <Card>
                         <CardContent>
-                            <Typography paragraph>여기에 원하는 정보를 나타내세요.</Typography>
+                            <Typography paragraph>(하천 수위에 대한 정보)</Typography>
                         </CardContent>
                     </Card>
                 </Collapse>

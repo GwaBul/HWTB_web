46b623a110cedb531822ae11182ad44fcbed57de
yang <junsuk530@gmail.com>
2024-05-26 20:27:21 +0900

    알림 누르면 실행

--- src/components/service/CitiesService.js
+++ src/components/service/CitiesService.js
@@ -11,7 +11,6 @@ const CitiesService = () => {
         if (registration && registration.active) {
           // 활성화된 Service Worker에 대한 메시지 리스너 추가
           registration.active.addEventListener('message', (event) => {
-            console.log(3);
             console.log('메인 스크립트로부터 메시지 수신:', event.data);
             if (event.data.type === 'CITIES_UPDATE') {
               setCities(event.data.data);
